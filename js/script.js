2ec94578b8de506032a1ebbd3d791e4341e8d581
Vaiker <vankad24@mail.ru>
2022-06-01 17:20:33 +0800

    Import menu

--- js/script.js
+++ js/script.js
@@ -1,16 +1,27 @@
-function hi(){
-    alert("hi")
+function fillContent(selector, fromFile){
+    fetch(fromFile)
+        .then((response) => {
+            // console.log("resp " + response);
+            return response.text();
+        })
+        .then((data) => {
+            document.querySelector(selector).innerHTML = data
+            console.log("filled content "+data)
+        });
+
 }
+
+fillContent(".menu","menu.html")
 
 main_height = document.getElementsByClassName("main-div")[0]
 fog = document.getElementById("fog")
 tog = document.getElementById("toggle")
-// fog.height = main_height
 
-tog.addEventListener("click",()=> fog.classList.toggle('show') )
-fog.onclick = ()=>tog.click()
+tog.addEventListener("click", () => fog.classList.toggle('show'))
+fog.onclick = () => tog.click()
 
-function test(ev){
+
+/*function test(ev){
     // el = document.getElementById("test")
     // el.innerText = Math.random()
     document.getElementById("toggle")
@@ -19,4 +30,5 @@ document.querySelector("input[value=\"Hello\"]").onclick = test
 function print(event){
     console.log(window.innerWidth+" "+window.outerWidth)
     // console.log(event)
-}
+}*/
+
